c38cc3f8af63a103e87aa99f04b29be6933130af
hendisantika <hendisantika@yahoo.co.id>
2023-04-18 21:39:38 +0700

    #7 - Add UserDetailsImpl ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootjwtdockerpostgresql/model/UserDetailsImpl.java
@@ -0,0 +1,75 @@
+package com.hendisantika.springbootjwtdockerpostgresql.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-docker-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/18/23
+ * Time: 21:38
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private final Long id;
+
+    private final String phoneNumber;
+
+    @JsonIgnore
+    private final String password;
+
+    public UserDetailsImpl(Long id, String phoneNumber, String password) {
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getPhoneNumber(),
+                user.getPassword());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
