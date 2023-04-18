fcd751fa79978d5c24728984e752c5234f06bc99
hendisantika <hendisantika@yahoo.co.id>
2023-04-18 21:47:37 +0700

    #19 - Add JwtUtils ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootjwtdockerpostgresql/jwt/JwtUtils.java
+++ src/main/java/com/hendisantika/springbootjwtdockerpostgresql/jwt/JwtUtils.java
@@ -4,10 +4,17 @@ import com.hendisantika.springbootjwtdockerpostgresql.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,7 +39,29 @@ public class JwtUtils {
     @Autowired
     private UserRepository userRepository;
 
-    private RSAPublicKey rsaPublicKey;
+    private final RSAPublicKey rsaPublicKey;
 
-    private RSAPrivateKey rsaPrivateKey;
+    private final RSAPrivateKey rsaPrivateKey;
+
+    public JwtUtils() throws Exception {
+        Resource publicKeyResource = new ClassPathResource("public.txt");
+        byte[] publicKey = Base64.getDecoder().decode(
+                publicKeyResource.getContentAsString(StandardCharsets.UTF_8)
+                        .replace("-----BEGIN PUBLIC KEY-----", "")
+                        .replace("-----END PUBLIC KEY-----", "")
+                        .replaceAll("\\R", "")
+        );
+
+        Resource privateKeyResource = new ClassPathResource("private.txt");
+        byte[] privateKey = Base64.getDecoder().decode(
+                privateKeyResource.getContentAsString(StandardCharsets.UTF_8)
+                        .replace("-----BEGIN PRIVATE KEY-----", "")
+                        .replace("-----END PRIVATE KEY-----", "")
+                        .replaceAll("\\R", "")
+        );
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        rsaPublicKey = (RSAPublicKey) keyFactory.generatePublic(new X509EncodedKeySpec(publicKey));
+        rsaPrivateKey = (RSAPrivateKey) keyFactory.generatePrivate(new PKCS8EncodedKeySpec(privateKey));
+    }
 }
