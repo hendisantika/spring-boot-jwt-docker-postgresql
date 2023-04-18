4917cd206bda6737028267f14f0b1f4624c623ce
hendisantika <hendisantika@yahoo.co.id>
2023-04-18 21:43:36 +0700

    #11 - Add SignupRequest Request Payload ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootjwtdockerpostgresql/payload/request/SignupRequest.java
@@ -0,0 +1,36 @@
+package com.hendisantika.springbootjwtdockerpostgresql.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-docker-postgresql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/18/23
+ * Time: 21:43
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 10, max = 13)
+    @Pattern(regexp = "^08\\d{10,13}$", message = "Phone number mandatory min 10 max 13 must started with 08")
+    private String phoneNumber;
+
+    @NotBlank
+    @Size(max = 60, message = "Name mandatory max 60")
+    private String name;
+
+    @NotBlank
+    @Size(min = 6, max = 16)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{6,16}$", message = "Password mandatory min 6, max 16, containing at least 1 capital letter\n" +
+            "and 1 number.")
+    private String password;
+}
